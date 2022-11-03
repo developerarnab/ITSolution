b71fdc9f2a0e80176e961b8a782a79a27c2d5b91
developerarnab <115336244+developerarnab@users.noreply.github.com>
2022-11-04 04:34:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const itech = document.querySelector(".itech");
+
+const navsmenu = document.querySelector(".navsmenu");
+
+itech.addEventListener("click", () => {
+
+    itech.classList.toggle("active");
+     
+    navsmenu.classList.toggle("active");
+
+}
+)
+// -preload
+
+
+
+
+
+
+
+
+
+
+
+
